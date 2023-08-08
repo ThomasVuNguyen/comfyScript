321e7069abced5a049deb949c19536eb2e24bcbd
Thomas the Maker <nnt0001@uah.edu>
2023-08-07 20:10:02 -0400

    Servo script ready
    Servo script ready
    To run:
    
    python3 servo/angle.py pinNum servoAngle
    
    where pinNum is the pinout for servo connection & servoAngle is the desired angle

--- servo/angle.py
+++ servo/angle.py
@@ -1,6 +1,22 @@
 from gpiozero import AngularServo
+import RPi.GPIO as GPIO
 from time import sleep
+import sys
+servoPin = int(sys.argv[1])
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(servoPin, GPIO.OUT)
+pwm = GPIO.PWM(servoPin, 50)
+pwm.start(0)
 
-servo = AngularServo(4, min_angle=0, max_angle=180)
-angle = 90
-servo.angle=angle
+def SetAngle(angle):
+    duty = angle/18+2
+    GPIO.output(servoPin,True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(servoPin, False)
+    pwm.ChangeDutyCycle(0)
+
+SetAngle(int(sys.argv[2]))
+
+#to use run command: python3 angle.py pinNum servoAngle
