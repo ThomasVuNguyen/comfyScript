7c0843c53d2d5dee929d45c838023d2ed1145607
Thomas the Maker <nnt0001@uah.edu>
2023-09-04 16:17:39 -0400

    Update HC-SR04.py
    Changing distance sensor class method from a while Loop to ...no while Loop...
    This allows for better integration with Stream reader in Dart

--- distance_sensor/HC-SR04.py
+++ distance_sensor/HC-SR04.py
@@ -9,36 +9,38 @@ class sensor:
         self.echo = echo 
     def setup(self):
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         GPIO.setup(self.trig, GPIO.OUT)
         GPIO.setup(self.echo, GPIO.IN)
         GPIO.output(self.trig, GPIO.LOW)
     def cleanup(self):
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         GPIO.output(self.trig, GPIO.LOW)
         GPIO.cleanup()
     def disable(self):
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         GPIO.setup(self.trig, GPIO.IN)
         time.sleep(0.01)
         GPIO.cleanup()
     def read_distance(self, state):
         self.setup()
-        while True:
-            GPIO.output(self.trig, GPIO.HIGH)
-            time.sleep(reading_rate)
-            GPIO.output(self.trig, GPIO.LOW)
-            if state==0:
-                self.disable()
-                exit(1)
-            while GPIO.input(self.echo) ==0 and GPIO.gpio_function(self.trig)==0:
-                pulse_start_time = time.time()
-            while GPIO.input(self.echo) ==1 and GPIO.gpio_function(self.trig)==0:
-                pulse_end_time = time.time()
-            if GPIO.gpio_function(self.trig)==1:
-                exit(1)
-            pulse_duration = pulse_end_time - pulse_start_time
-            distance_cm = round(pulse_duration * 17150, 2)
-            print(distance_cm)
+        GPIO.output(self.trig, GPIO.HIGH)
+        time.sleep(reading_rate)
+        GPIO.output(self.trig, GPIO.LOW)
+        if state==0:
+            self.disable()
+            exit(1)
+        while GPIO.input(self.echo) ==0 and GPIO.gpio_function(self.trig)==0:
+            pulse_start_time = time.time()
+        while GPIO.input(self.echo) ==1 and GPIO.gpio_function(self.trig)==0:
+            pulse_end_time = time.time()
+        if GPIO.gpio_function(self.trig)==1:
+            exit(1)
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance_cm = round(pulse_duration * 17150, 2)
+        print(distance_cm)
 
 trig = int(sys.argv[1])
 echo = int(sys.argv[2])
