51bf47033986e0391113dfd85e3d94acff938537
huzknowgit <muhammad.farooq8@student.tdsb.on.ca>
2024-08-01 00:03:26 -0400

    Update sg90.py
    fixes the problem that was in the servo code.

--- servo/sg90.py
+++ servo/sg90.py
@@ -1,37 +1,47 @@
+#!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
 import time
 import sys
 
-class servo:
+class Servo:
     def __init__(self, pin):
         self.pin = pin
-    def cleanup(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.cleanup()
-    def setupMotor(self):
+        self.setup_motor()
+
+    def setup_motor(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.OUT)
-        pass
-    def disable(self):
+        self.servo_pwm = GPIO.PWM(self.pin, 50)  # 50Hz frequency
+        self.servo_pwm.start(0)
+
+    def cleanup(self):
+        self.servo_pwm.stop()
         GPIO.cleanup()
+
     def rotate(self, angle):
-        self.setupMotor()
-        servoRun = GPIO.PWM(self.pin, 50)
-        servoRun.start(0)
-        try:
-            while True:
-                duty = angle/18+2
-                servoRun.ChangeDutyCycle(duty)
-                time.sleep(0.4)
-                servoRun.ChangeDutyCycle(0)
-                break
-        finally:
-            servoRun.stop()
-            self.disable()
-pin = int(sys.argv[1])
-angle = int(sys.argv[2])
+        if angle < 0 or angle > 180:
+            print("Angle must be between 0 and 180 degrees")
+            return
 
-servo1 = servo(pin)
-servo1.rotate(angle%180)
+        duty = self.angle_to_duty_cycle(angle)
+        self.servo_pwm.ChangeDutyCycle(duty)
+        time.sleep(0.5)
+        self.servo_pwm.ChangeDutyCycle(0)  # Stop sending signal
 
+    def angle_to_duty_cycle(self, angle):
+        return 2 + (angle / 18.0)
+
+    def __del__(self):
+        self.cleanup()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 servo.py <pin> <angle>")
+        sys.exit(1)
+
+    pin = int(sys.argv[1])
+    angle = int(sys.argv[2])
+
+    servo = Servo(pin)
+    servo.rotate(angle)
